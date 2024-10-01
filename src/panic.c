f033363e651f6e65fe4231ecc187781a3bfcd06d
Mariusz Łapkowski <171329112+lapkowski@users.noreply.github.com>
2024-10-01 10:24:59 +0200

    Replaced the PANIC_FUNCTION macro with a panic_set_panic_function function.

--- src/panic.c
+++ src/panic.c
@@ -20,8 +20,14 @@
 
 #include <stdbool.h>
 
-___PanicFunction panic_function = &PANIC_FUNCTION;
+PanicFunction panic_function = &PANIC_FUNCTION;
 bool panic_exit_on_panic = true;
+
+void panic_set_panic_function(PanicFunction new)
+{
+    if (new == NULL) panic("Api abuse on panic_set_panic_function (new == NULL).");
+    panic_function = new;
+}
 
 void ___default_panic(RESULT_PANIC_FUNCTION_PARAMTETERS)
 {
